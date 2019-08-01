eacbb4c52738db121c7c36c587151cb01036a7af
Srikalyan Megati <kalmegati@gmail.com>
2019-08-01 03:52:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,10 @@
-def $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-end
-
-
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  def status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
